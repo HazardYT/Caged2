040e73db50fcc01022f8c6b9ded5755fb6bd09f5
Barbra <joshturbessi@gmail.com>
2023-07-17 19:59:40 -0500

    Update CharacterSelectState.cs

--- Caged2/Assets/_Scripts/CharacterSelectState.cs
+++ Caged2/Assets/_Scripts/CharacterSelectState.cs
@@ -1,23 +1,28 @@
 using System;
+using Unity.Collections;
 using Unity.Netcode;
 public struct CharacterSelectState : INetworkSerializable, IEquatable<CharacterSelectState>
 {
     public ulong ClientId;
     public int CharacterId;
+    public FixedString32Bytes name;
 
-    public CharacterSelectState(ulong clientId, int characterId = -1)
+    public CharacterSelectState(ulong clientId, FixedString32Bytes steamName = default, int characterId = -1)
     {
+        name = steamName;
         ClientId = clientId;
         CharacterId = characterId;
     }
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
     {
+        serializer.SerializeValue(ref name);
         serializer.SerializeValue(ref ClientId);
         serializer.SerializeValue(ref CharacterId);
     }
     public bool Equals(CharacterSelectState other)
     {
         return ClientId == other.ClientId &&
-            CharacterId == other.CharacterId;
+            CharacterId == other.CharacterId && 
+            name == other.name;
     }
 }
