ce1f2a3d5914b05ffe6f727b2768ff262669c943
Barbra <joshturbessi@gmail.com>
2023-07-21 21:27:14 -0500

    Update Inventory.cs

--- Caged2/Assets/_Scripts/Inventory/Inventory.cs
+++ Caged2/Assets/_Scripts/Inventory/Inventory.cs
@@ -18,93 +18,133 @@ public class Inventory : NetworkBehaviour
     {
         ManageInput();
     }
-    private void ManageInput(){
 
-        // Keyboard and Universal
-        if (UserInput.instance.InteractPressed){
+    // Process player input
+    private void ManageInput()
+    {
+        // Keyboard and Universal Input
+        if (UserInput.instance.InteractPressed)
+        {
             RaycastToItem();
         }
-        if (UserInput.instance.RightHandPressed){
-            if (_handItems[0] != null){
+
+        if (UserInput.instance.RightHandPressed && _handItems[0] != null)
+        {
             SelectHandServerRpc(0);
-            }
         }
-        if (UserInput.instance.LeftHandPressed){
-            if (_handItems[1] != null){
+
+        if (UserInput.instance.LeftHandPressed && _handItems[1] != null)
+        {
             SelectHandServerRpc(1);
-            }
         }
-        if (UserInput.instance.ThrowHeld){
+
+        if (UserInput.instance.ThrowHeld)
+        {
+            // Perform any actions when the throw button is held down.
         }
-        if (UserInput.instance.ThrowReleased){
+
+        if (UserInput.instance.ThrowReleased)
+        {
             DropSelectedItemServerRpc();
         }
 
-        //Controller Specific Input
-
-        if (UserInput.instance.RightBumperPressed){
-
+        // Controller Specific Input
+        if (UserInput.instance.RightBumperPressed)
+        {
+            // Perform any actions when the right bumper is pressed.
         }
-        if (UserInput.instance.LeftBumperPressed){
 
+        if (UserInput.instance.LeftBumperPressed)
+        {
+            // Perform any actions when the left bumper is pressed.
         }
     }
-    private void RaycastToItem(){
-        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, 5, layerMask)){
-            if (hit.transform.CompareTag("Item")){
+
+    // Raycast to detect and pick up items in the scene
+    private void RaycastToItem()
+    {
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, 5, layerMask))
+        {
+            if (hit.transform.CompareTag("Item"))
+            {
                 for (byte i = 0; i < _handItems.Length; i++)
                 {
                     if (_handItems[i] != null) continue;
-                        hit.transform.TryGetComponent<NetworkObject>(out NetworkObject networkObject);        
+
+                    if (hit.transform.TryGetComponent<NetworkObject>(out NetworkObject networkObject))
+                    {
                         NetworkObjectReference reference = new NetworkObjectReference(networkObject);
-                        PickupItemServerRpc(i,reference);
+                        PickupItemServerRpc(i, reference);
                         return;
                     }
                 }
+
                 Debug.LogError("Your Hands are Full!");
             }
-    
+        }
     }
+
+    // Server RPC to pick up an item and place it in the player's hand
     [ServerRpc]
-    public void PickupItemServerRpc(byte slot,NetworkObjectReference networkObjectReference, ServerRpcParams rpcParams = default){
+    public void PickupItemServerRpc(byte slot, NetworkObjectReference networkObjectReference, ServerRpcParams rpcParams = default)
+    {
         Inventory inv = NetworkManager.ConnectedClients[rpcParams.Receive.SenderClientId].PlayerObject.GetComponent<Inventory>();
-        if (!networkObjectReference.TryGet(out NetworkObject networkObject)){
-            Debug.LogError("Failed to Pickup item Because:  TryGet from NetworkObjectReference Failed.");
+        if (!networkObjectReference.TryGet(out NetworkObject networkObject))
+        {
+            Debug.LogError("Failed to Pickup item Because: TryGet from NetworkObjectReference Failed.");
+            return;
         }
-        if (!networkObject.TrySetParent(inv.transform, false)){
-            Debug.LogError("Failed to Pickup item Because:  TrySetParent Failed.");
+
+        if (!networkObject.TrySetParent(inv.transform, false))
+        {
+            Debug.LogError("Failed to Pickup item Because: TrySetParent Failed.");
+            return;
         }
+
         _handItems[slot] = networkObject.transform;
         var pickUpObjectRigidbody = networkObject.GetComponent<Rigidbody>();
         pickUpObjectRigidbody.isKinematic = true;
         pickUpObjectRigidbody.interpolation = RigidbodyInterpolation.None;
-        ConstraintSource constraintSource = new ConstraintSource {
-            sourceTransform = _handSlots[slot], weight = 1
+        ConstraintSource constraintSource = new ConstraintSource
+        {
+            sourceTransform = _handSlots[slot],
+            weight = 1
         };
         ParentConstraint constraint = networkObject.GetComponent<ParentConstraint>();
         constraint.AddSource(constraintSource);
         constraint.constraintActive = true;
         SelectHandServerRpc(slot);
     }
+
+    // Server RPC to select the item in the player's hand
     [ServerRpc]
     public void SelectHandServerRpc(byte value, ServerRpcParams rpcParams = default)
-    {   
+    {
         Inventory inv = NetworkManager.ConnectedClients[rpcParams.Receive.SenderClientId].PlayerObject.GetComponent<Inventory>();
-        if (inv._selectedHandItem != null){
+        if (inv._selectedHandItem != null)
+        {
             inv._selectedHandItem.localScale /= 1.25f;
             inv._selectedHandItem = null;
         }
+
         inv._selectedHandItem = _handItems[value];
         inv._selectedSlot = value;
         inv._selectedHandItem.localScale *= 1.25f;
     }
+
+    // Server RPC to drop the selected item from the player's hand
     [ServerRpc]
-    public void DropSelectedItemServerRpc(ServerRpcParams rpcParams = default){
+    public void DropSelectedItemServerRpc(ServerRpcParams rpcParams = default)
+    {
         Inventory inv = NetworkManager.ConnectedClients[rpcParams.Receive.SenderClientId].PlayerObject.GetComponent<Inventory>();
-        if (inv._selectedHandItem != null){ 
-            if (!inv._handItems[inv._selectedSlot].GetComponent<NetworkObject>().TryRemoveParent(true)){
-                Debug.LogError("Failed to Drop item Because:  TryRemoveParent Failed.");
+        if (inv._selectedHandItem != null)
+        {
+            if (!inv._handItems[inv._selectedSlot].GetComponent<NetworkObject>().TryRemoveParent(true))
+            {
+                Debug.LogError("Failed to Drop item Because: TryRemoveParent Failed.");
+                return;
             }
+
             inv._selectedHandItem.localScale /= 1.25f;
             var pickUpObjectRigidbody = inv._handItems[inv._selectedSlot].GetComponent<Rigidbody>();
             pickUpObjectRigidbody.isKinematic = false;
@@ -117,15 +157,15 @@ public class Inventory : NetworkBehaviour
             pickUpObjectRigidbody.AddForce(transform.forward * 2, ForceMode.Impulse);
             for (byte i = 0; i < _handItems.Length; i++)
             {
-                if (_handItems[i] != null){
+                if (_handItems[i] != null)
+                {
                     SelectHandServerRpc(i);
                 }
             }
         }
-        else{
+        else
+        {
             Debug.LogError("You have no Selected items to drop!");
         }
     }
-
-
 }
